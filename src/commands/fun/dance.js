ab82944b2bb62237ac2621fe208d3d892a03dc39
Gming2 <112753555+Gming2@users.noreply.github.com>
2023-06-14 22:04:09 +0100

    Update dance.js

--- src/commands/fun/dance.js
+++ src/commands/fun/dance.js
@@ -28,6 +28,9 @@ module.exports = {
         'emote-pose1',
         'emote-pose8',
         'emote-pose7'
+        'dance-russian'
+        'emote-energyball'
+        'dance-blackpink'
       ]
       const randomEmote = emotes[Math.floor(Math.random() * emotes.length)];
       const option = args[0];
