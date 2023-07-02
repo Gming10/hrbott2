aed8ab6b6371a117a66add8d378d25c75d62386a
Gming2 <112753555+Gming2@users.noreply.github.com>
2023-07-02 20:04:26 +0100

    Update playerJoin.js

--- src/events/server/playerJoin.js
+++ src/events/server/playerJoin.js
@@ -9,6 +9,6 @@ module.exports = {
       logJoin(user);
       console.log(`Bienvenue à vous ${user.username}(${user.id} faites comme chez vous`);
     }
-    bot.message.send(`Bienvenue à vous ${user.username} faites comme chez vous`)
+    bot.message.send(`Bienvenue à vous ${user.username}, amusez vous bien !`)
   }
 };
